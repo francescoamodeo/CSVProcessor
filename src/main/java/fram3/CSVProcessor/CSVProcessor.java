dc6cbdd2a4a97bf91885149ffcee42509b93c227
francescoamodeo <francescoamodeo99@gmail.com>
2024-05-23 19:27:58 +0200

    added documentation

--- src/main/java/fram3/CSVProcessor/CSVProcessor.java
+++ src/main/java/fram3/CSVProcessor/CSVProcessor.java
@@ -1,10 +1,47 @@
 package fram3.CSVProcessor;
 
 public interface CSVProcessor {
+    /**
+     * sort csv data using the properties specified one by one
+     * @param properties properties used to sort
+     * @throws IllegalArgumentException if one of the properties is not found
+     */
+    void sortBy(String[] properties) throws IllegalArgumentException;
 
-    void sortBy(String[] properties);
-    void filter(FilterCondition condition);
-    void writeChanges();
+    /**
+     * filter csv data using a logical condition formed by AND and OR operators and Filter expressions
+     * @param condition logical condition of filters
+     * @throws IllegalArgumentException if one filter is malformed
+     */
+    void filter(FilterCondition condition) throws IllegalArgumentException;
+
+    /**
+     * notarize the output file of CSVProcessor timestamping with OpenTimestamps
+     */
     void notarize();
+
+    /**
+     * notarize the output file of CSVProcessor timestamping with OpenTimestamps
+     * @param fileName file name of the file to be notarized
+     */
+    void notarize(String fileName);
+
+    /**
+     * read new csv data
+     * @param inputFileName file name used read to read csv data
+     * @throws IllegalArgumentException if file is not found
+     */
+    void readData(String inputFileName) throws IllegalArgumentException;
+
+    /**
+     * write new csv data
+     * @param outputFileName file name used to write csv data
+     */
+    void writeData(String outputFileName);
+
+    /**
+     * write partial changes made with CSVProcessor
+     */
+    void writeChanges();
 
 }
